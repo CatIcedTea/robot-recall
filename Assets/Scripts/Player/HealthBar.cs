3751a483e3fb1d49c03cc43fd7f19619d269aa9b
EthanRossJ <51828783+EthanRossJ@users.noreply.github.com>
2024-10-03 15:05:31 -0400

    HealthStart
    Some Preliminary work on the Health and UI systems

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //Implement Health and Healthbar functionality here
-    void Start()
+    [SerializeField]
+    private Health _health;
+
+    [SerializeField]
+    private RectTransform _barRect;
+
+    [SerializeField]
+    private RectMask2D _mask;
+
+    [SerializeField]
+    private TMP_Text _hpIndicator;
+
+    private float _maxRightMask;
+    private float _initialRightMask;
+    private void Start()
     {
-        
+        _maxRightMask = _barRect.rect.width - _mask.padding.x - _mask.padding.z;
+        _hpIndicator.SetText(sourceText: $"{_health.Hp}/{_health.MaxHp}");
+        _initialRightMask = _mask.padding.z;
+    }
+    public void SetValue(int newValue)
+    {
+        var targetWidth = newValue * _maxRightMask / _health.MaxHp;
+        var newRightMask = _maxRightMask + _initialRightMask - targetWidth;
+        var padding = _mask.padding;
+        padding.z = newRightMask;
+        _mask.padding = padding;
+        _hpIndicator.SetText($"{newValue}/{_health.MaxHp}");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
