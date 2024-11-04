967ba9b56cd6df8a5e5e5de6832727677e0bbf94
rfox985 <ryandafox55@gmail.com>
2024-11-04 16:41:15 -0500

    .

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Door : MonoBehaviour
+{
+    public GameObject doorClosed, doorOpened, intIcon;
+    void onTriggerStay(Collider other){
+        
+    }
+}
