d63eac5af2ff708dc00a379fcd8645f645b88485
EthanRossJ <51828783+EthanRossJ@users.noreply.github.com>
2024-12-01 02:45:12 -0500

    Current Edit
    UI improvement and End + Reset Screen

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -1,14 +1,52 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Hole : MonoBehaviour
 {
+    public GameObject endScoreMenu;
+    public static bool GameEndMenuActive;
+    //public static int scoreValue = 0;
+    public TextMeshProUGUI score;
+
     private void OnTriggerEnter(Collider other)
     {
+        GameEndMenuActive = false;
+
         if (other.CompareTag("Player")) // Ensure the Player has the "Player" tag
         {
+            endScoreMenu.SetActive(true);
+            score.text = "Final Score:" + ScoreScript.scoreValue.ToString();
+            Time.timeScale = 0f;
             Debug.Log("Player fell through the hole!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            GameEndMenuActive=true;
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+
+                Application.Quit();
+
+            }
         }
     }
+    void Update()
+    {
+        score.text = "Final Score:" + ScoreScript.scoreValue.ToString();
+        if (Input.GetKeyDown(KeyCode.R) && GameEndMenuActive)
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && GameEndMenuActive)
+        {
+
+            Application.Quit();
+
+        }
+    }
+
+
 }
